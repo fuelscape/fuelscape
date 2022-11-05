cf1f9694cf62fc5075cfef20853ca1c1aa5041da
Max Wolter <awfm9@protonmail.com>
2022-11-05 09:06:40 -0700

    Fix first backend version

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,10 +1,15 @@
+#[macro_use]
+extern crate lazy_static;
+
 use std::collections::HashMap;
+use std::io::Error;
+use std::io::ErrorKind;
+use std::sync::Mutex;
 
 use serde::Deserialize;
+use serde::Serialize;
 
-use poem::error::Error;
 use poem::error::Conflict;
-use poem::get;
 use poem::handler;
 use poem::listener::TcpListener;
 use poem::post;
@@ -15,47 +20,53 @@ use poem::web::Json;
 
 use fuel_gql_client::fuel_tx::Address;
 
-#[derive(Deserialize,Serialize)]
-struct Link {
+lazy_static! {
+    static ref WALLET_LOOKUP: Mutex<HashMap<String, Address>> = {
+        let lookup = HashMap::new();
+        Mutex::new(lookup)
+    };
+}
+
+
+#[derive(Deserialize)]
+struct CreateLinkRequest {
     player: String,
     wallet: Address,
 }
 
-struct LinkController {
-    lookup: HashMap<String, Address>,
+#[derive(Serialize)]
+struct CreateLinkResponse {
+    player: String,
+    wallet: Address,
 }
 
-impl LinkController {
-    #[handler]
-    fn create(&self, req: Json<Link>) -> Result<Json<Link>> {
-        match self.lookup.get(req.player) {
-            Some(wallet) => Err(Conflict(String::from("player already linked to wallet"))),
-            _ => (),
-        }
-    
-        self.lookup.insert(req.player, req.wallet);
-    
-        Ok(Json(req))
+#[handler]
+fn create_link(req: Json<CreateLinkRequest>) -> Result<Json<CreateLinkResponse>> {
+
+    let mut lookup = WALLET_LOOKUP.lock().unwrap();
+    match lookup.get(&req.player) {
+        Some(_) => return Err(Conflict(Error::new(
+                ErrorKind::AlreadyExists,
+                "player already linked to wallet",
+            ))),
+        None => (),
     }
 
-    #[handler]
-    fn list(&self) -> Result<Json<Vec<Link>>> {
-    
-    }
+    lookup.insert(req.player.clone(), req.wallet);
+
+    let res = CreateLinkResponse{
+        player: req.player.clone(),
+        wallet: req.wallet,
+    };
+
+    Ok(Json(res))
 }
 
 #[tokio::main]
-async fn main() -> Result<(),Error> {
-
-    let lookup = HashMap::new();
-
-    let links = LinkController{
-        lookup: lookup,
-    };
+async fn main() -> Result<(),std::io::Error> {
 
     let app = Route::new()
-        .at("/links/", post(links.create))
-        .at("/links/", get(links.list));
+        .at("/links/", post(create_link));
 
     Server::new(TcpListener::bind("127.0.0.1:8080"))
         .run(app)
