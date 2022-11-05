b63e96446fea4feb4447f8fb74b78dd11b8f2aad
Juan Palomino <46542939+J-Palomino@users.noreply.github.com>
2022-11-05 02:36:32 -0700

    Feat react app (#3)
    * feat: front end scaffodl
    
    * chore: reorg folders:
    
    * working localstorage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,38 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { Wallet } from "fuels";
 
 function App() {
-  console.log(Wallet.generate());
+  const localWallet = localStorage.getItem('ActiveWallet')
+  const [ActiveWallet, setActiveWallet] = useState('');
+  const [isReturnUser, setReturnUser] = useState(false);
+  console.log("localWallet", localWallet)
+
+   useEffect(() => {
+    if (localWallet){
+setActiveWallet(localWallet);
+setReturnUser(true);
+    }
+  
+  },[]);
+  
+// if (localWallet !== (null && ActiveWallet)){setActiveWallet(localWallet)};
+  let newWallet = () => {
+    let newWallet = JSON.stringify(Wallet.generate().address)
+   setActiveWallet(newWallet);
+   localStorage.setItem('ActiveWallet', newWallet);
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() =>  newWallet()}>
+          Generate Random Wallet
+        </button>
+        {isReturnUser && <p> Welcome Back! </p>}
+        {ActiveWallet && <p>{ActiveWallet}</p>}
+        
       </header>
     </div>
   );
