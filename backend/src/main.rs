5f987cee0de7d33ffa47f34e341317eece42e372
Max Wolter <awfm9@protonmail.com>
2022-11-05 09:53:49 -0700

    Switch to Bech32 addresses on API

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -5,11 +5,13 @@ use std::collections::HashMap;
 use std::io::Error;
 use std::io::ErrorKind;
 use std::sync::Mutex;
+use std::str::FromStr;
 
 use serde::Deserialize;
 use serde::Serialize;
 
 use poem::error::BadGateway;
+use poem::error::BadRequest;
 use poem::error::Conflict;
 use poem::error::InternalServerError;
 use poem::error::NotFound;
@@ -21,18 +23,20 @@ use poem::Route;
 use poem::Server;
 use poem::web::Json;
 
+use fuel_crypto::PublicKey;
 use fuel_crypto::SecretKey;
 use fuels_signers::provider::Provider;
-use fuels_signers::WalletUnlocked;
+// use fuels_signers::WalletUnlocked;
 use fuels_signers::wallet::DEFAULT_DERIVATION_PATH_PREFIX;
-use fuel_gql_client::fuel_tx::Address;
+use fuels_types::bech32::FUEL_BECH32_HRP;
+use fuels_types::bech32::Bech32Address;
 
+const API_PORT: &str = "8080";
 const NODE_URL: &str = "node-beta-1.fuel.network";
-
 const WALLET_MNEMONIC: &str = "wet person force drum vicious milk afraid target treat verify faculty dilemma forget across congress visa hospital skull twenty sick ship tent limit survey";
 
 lazy_static! {
-    static ref WALLET_LOOKUP: Mutex<HashMap<String, Address>> = {
+    static ref WALLET_LOOKUP: Mutex<HashMap<String, Bech32Address>> = {
         let lookup = HashMap::new();
         Mutex::new(lookup)
     };
@@ -41,17 +45,22 @@ lazy_static! {
 #[derive(Deserialize)]
 struct CreateLinkRequest {
     player: String,
-    wallet: Address,
+    wallet: String,
 }
 
 #[derive(Serialize)]
 struct CreateLinkResponse {
     player: String,
-    wallet: Address,
+    wallet: String,
 }
 
 #[handler]
 fn create_link(req: Json<CreateLinkRequest>) -> Result<Json<CreateLinkResponse>> {
+
+    let wallet = match Bech32Address::from_str(&req.wallet) {
+        Ok(wallet) => wallet,
+        Err(err) => return Err(BadRequest(err)),
+    };
 
     let mut lookup = WALLET_LOOKUP.lock().unwrap();
     match lookup.get(&req.player) {
@@ -62,11 +71,11 @@ fn create_link(req: Json<CreateLinkRequest>) -> Result<Json<CreateLinkResponse>>
         None => (),
     }
 
-    lookup.insert(req.player.clone(), req.wallet);
+    lookup.insert(req.player.clone(), wallet);
 
     let res = CreateLinkResponse{
         player: req.player.clone(),
-        wallet: req.wallet,
+        wallet: req.wallet.clone(),
     };
 
     Ok(Json(res))
@@ -80,8 +89,7 @@ struct CreateKillRequest {
 
 #[derive(Serialize)]
 struct CreateKillResponse {
-    player: String,
-    mob: u64,
+    address: String,
 }
 
 #[handler]
@@ -116,11 +124,14 @@ async fn create_kill(req: Json<CreateKillRequest>) -> Result<Json<CreateKillResp
         Err(err) => return Err(InternalServerError(err)),
     };
 
-    let unlocked = WalletUnlocked::new_from_private_key(secret, Some(provider));
+    // let unlocked = WalletUnlocked::new_from_private_key(secret, Some(provider));
+
+    let public = PublicKey::from(&secret);
+    let hashed = public.hash();
+    let address = Bech32Address::new(FUEL_BECH32_HRP, hashed);
 
     let res = CreateKillResponse{
-        player: req.player.clone(),
-        mob: req.mob,
+        address: address.to_string(),
     };
 
     Ok(Json(res))
@@ -130,9 +141,11 @@ async fn create_kill(req: Json<CreateKillRequest>) -> Result<Json<CreateKillResp
 async fn main() -> Result<(),std::io::Error> {
 
     let app = Route::new()
-        .at("/links/", post(create_link));
+        .at("/links/", post(create_link))
+        .at("/kills/", post(create_kill));
 
-    Server::new(TcpListener::bind("127.0.0.1:8080"))
+    let url = format!("127.0.0.1:{}", API_PORT);
+    Server::new(TcpListener::bind(url))
         .run(app)
         .await
 }
