7e2b2394a82bcff6c9d2a99cae784366b0777e4f
Juan Palomino <jpalomino09@gmail.com>
2022-11-05 14:26:13 -0700

    working account generation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,38 +1,52 @@
 import React, {useState, useEffect} from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { Wallet } from "fuels";
+import { Wallet, WalletUnlocked } from "@fuel-ts/wallet"
+import {AbstractWallet} from "@fuel-ts/interfaces"
+import { WalletManager } from "@fuel-ts/wallet-manager"
+
 
 function App() {
   const localWallet = localStorage.getItem('ActiveWallet')
-  const [ActiveWallet, setActiveWallet] = useState('');
+   const pvtKey = localStorage.getItem('PvtKey')
+  const [ActiveWallet, setActiveWallet] = useState<WalletUnlocked>()
   const [isReturnUser, setReturnUser] = useState(false);
+  const manager = new WalletManager();
   console.log("localWallet", localWallet)
+  console.log("pvtKey", pvtKey)
 
    useEffect(() => {
-    if (localWallet){
-setActiveWallet(localWallet);
-setReturnUser(true);
+    if (localWallet && pvtKey){
+    setActiveWallet(new WalletUnlocked("0x9d74ebdca29148547e0dd37e30adfec3e7988d061e7435892be934ef6809b190"));
+    setReturnUser(true);
+    console.log("activewallet: ", ActiveWallet)
     }
   
   },[]);
   
-// if (localWallet !== (null && ActiveWallet)){setActiveWallet(localWallet)};
-  let newWallet = () => {
-    let newWallet = JSON.stringify(Wallet.generate().address)
+  let createNewWallet = () => {
+    let newWallet = Wallet.generate()
    setActiveWallet(newWallet);
-   localStorage.setItem('ActiveWallet', newWallet);
+  localStorage.setItem('ActiveWallet', newWallet.address.toString());
+  localStorage.setItem('PvtKey', newWallet.privateKey)
+  }
+
+  let copyPvtKey = async() => {
+    if (ActiveWallet?.address){
+await window.navigator.clipboard.writeText(ActiveWallet?.privateKey);
+ alert("Copied pvt key to clipboard ")
+    }   
   }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <button onClick={() =>  newWallet()}>
-          Generate Random Wallet
+        <button onClick={() =>  createNewWallet()}>
+          New Wallet
         </button>
         {isReturnUser && <p> Welcome Back! </p>}
-        {ActiveWallet && <p>{ActiveWallet}</p>}
-        
+        {ActiveWallet &&<><h3> Active Wallet Address:</h3> <p>{JSON.stringify(ActiveWallet.address).slice(4, -1)}</p></>}
+        <button onClick={() =>  copyPvtKey()}> export pvt key</button>
       </header>
     </div>
   );
