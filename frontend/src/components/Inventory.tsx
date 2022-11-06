e95498e78f11beccf442f0b2297c0d0c1d42855d
Juan Palomino <jpalomino09@gmail.com>
2022-11-06 01:41:19 -0800

    updated inventory.tsx

--- frontend/src/components/Inventory.tsx
+++ frontend/src/components/Inventory.tsx
@@ -1,18 +1,19 @@
 import React, { useState, useEffect } from "react";
 interface InventoryRaw {
     player: string,
-  balances: Balances
+    wallet: string,
+  balances: Balance[]
 }
-interface Balances {
-itemID: number,
-amount: number
+interface Balance {
+item: number,
+balance: number
 }
 function Inventory() {
     // @ts-ignore
   const [inventory, setInventory] = useState<InventoryRaw>();
   let results = "someresults";
  useEffect(() => {
-    const url = "https://api.fuelscape.gg/items/fuel1cvwcrwsl09krl0dfqtttcemht483yux3t0mmh8xmpaz26edn6rrszg706m";
+    const url = "https://api.fuelscape.gg/items/test1";
     const fetchData = async () => {
       
       try {
@@ -30,17 +31,26 @@ function Inventory() {
 
     fetchData();
   }, []);
+  console.log("inventory: ", inventory)
 
-  let balanceArray = inventory?.balances ? Object.entries(inventory.balances) : null
-  console.log("balanceArr",balanceArray)
+
   return (
-    <>
-      <div className="Inventory-Wrapper">
-        <h1>Inventory:</h1>
-          {/* @ts-ignore */}
-        {balanceArray?.map(e => <div className="Inventory-Slot">{e?.map(e => <p>{e}</p>)}</div>)}
+   
+      <div className="Inventory-Wrapper"><h1>Inventory:</h1>
+      <div className="Slot-Wrapper">
+      <table>
+  <tr>
+    <th>Icon</th>
+    <th>Id</th>
+    <th>Name</th>
+    <th>Balance</th>
+  </tr>
+       {inventory?.balances?.map(e => <tr><td><img src={`https://www.osrsbox.com/osrsbox-db/items-icons/${e.item}
+      .png`}></img></td><td><p>{e?.item}</p></td><td><p>nombre</p></td><td><p>{e?.balance}</p></td></tr>)}
+</table>
       </div>
-    </>
+      </div>
+    
   );
 }
 
