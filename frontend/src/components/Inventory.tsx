e14c6925adfe52c633686b012702c16aadece289
Juan Palomino <jpalomino09@gmail.com>
2022-11-06 01:45:51 -0700

    feat: parsing inventory

--- frontend/src/components/Inventory.tsx
+++ frontend/src/components/Inventory.tsx
@@ -1,27 +1,28 @@
 import React, { useState, useEffect } from "react";
-
+interface InventoryRaw {
+    player: string,
+  balances: Balances
+}
+interface Balances {
+itemID: number,
+amount: number
+}
 function Inventory() {
-  const [inventory, setInventory] = useState("");
+    // @ts-ignore
+  const [inventory, setInventory] = useState<InventoryRaw>();
   let results = "someresults";
-
-  useEffect(() => {
-    const url = "https://api.fuelscape.gg/items/";
-
-const body = `{
-    "wallet": "fuel1cvwcrwsl09krl0dfqtttcemht483yux3t0mmh8xmpaz26edn6rrszg706m",
-    "item": 1323,
-    "amount": 1
-}`
-    const fetchData =async() => {
+ useEffect(() => {
+    const url = "https://api.fuelscape.gg/items/fuel1cvwcrwsl09krl0dfqtttcemht483yux3t0mmh8xmpaz26edn6rrszg706m";
+    const fetchData = async () => {
       
       try {
         const response = await fetch(url, {
-           method: "post", mode: 'no-cors',
-          headers: {"Content-Type": "application/json; charset=UTF-8" ,"Accept": "*/*"},
-          body:body
-        }).then(response => console.log(response)).then(data => {
-            console.log(data)
-        })
+          headers: {'Content-Type': 'application/json', 'Accept': 'application/json'},
+          method: "GET",
+        });
+        const json = await response.json();
+        console.log("json:",json);
+        setInventory(json);
       } catch (error) {
         console.log("error", error);
       }
@@ -30,12 +31,14 @@ const body = `{
     fetchData();
   }, []);
 
-
+  let balanceArray = inventory?.balances ? Object.entries(inventory.balances) : null
+  console.log("balanceArr",balanceArray)
   return (
     <>
       <div className="Inventory-Wrapper">
         <h1>Inventory:</h1>
-        <p>{results}</p>
+          {/* @ts-ignore */}
+        {balanceArray?.map(e => <div className="Inventory-Slot">{e?.map(e => <p>{e}</p>)}</div>)}
       </div>
     </>
   );
