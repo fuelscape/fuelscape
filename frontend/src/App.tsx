df8061d7d6593901d23c4c2daf66dfb9e5322f2e
Juan Palomino <jpalomino09@gmail.com>
2022-11-05 21:39:58 -0700

    add components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,62 +1,71 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { Wallet, WalletUnlocked } from "@fuel-ts/wallet"
-import {AbstractWallet} from "@fuel-ts/interfaces"
-import { WalletManager } from "@fuel-ts/wallet-manager"
+import { Wallet, WalletUnlocked } from "@fuel-ts/wallet";
+import { AbstractWallet } from "@fuel-ts/interfaces";
+import { WalletManager } from "@fuel-ts/wallet-manager";
 import { Routes, Route, Link } from "react-router-dom";
-import Market from "./components/Market"
-
+import Market from "./components/Market";
+import Inventory from "./components/Inventory";
 
 function App() {
-  const localWallet = localStorage.getItem('ActiveWallet')
-   const pvtKey = localStorage.getItem('PvtKey')
-  const [ActiveWallet, setActiveWallet] = useState<WalletUnlocked>()
+  const localWallet = localStorage.getItem("ActiveWallet");
+  const pvtKey = localStorage.getItem("PvtKey");
+  const [ActiveWallet, setActiveWallet] = useState<WalletUnlocked>();
   const [isReturnUser, setReturnUser] = useState(false);
   const manager = new WalletManager();
-  console.log("localWallet", localWallet)
-  console.log("pvtKey", pvtKey)
+  console.log("localWallet", localWallet);
+  console.log("pvtKey", pvtKey);
 
-   useEffect(() => {
-    if (localWallet && pvtKey){
-    setActiveWallet(new WalletUnlocked("0x9d74ebdca29148547e0dd37e30adfec3e7988d061e7435892be934ef6809b190"));
-    setReturnUser(true);
-    console.log("activewallet: ", ActiveWallet)
+  useEffect(() => {
+    if (localWallet && pvtKey) {
+      setActiveWallet(
+        new WalletUnlocked(
+          "0x9d74ebdca29148547e0dd37e30adfec3e7988d061e7435892be934ef6809b190"
+        )
+      );
+      setReturnUser(true);
+      console.log("activewallet: ", ActiveWallet);
     }
-  
-  },[]);
-  
-  let createNewWallet = () => {
-    let newWallet = Wallet.generate()
-   setActiveWallet(newWallet);
-  localStorage.setItem('ActiveWallet', newWallet.address.toString());
-  localStorage.setItem('PvtKey', newWallet.privateKey)
-  }
+  }, []);
 
-  let copyPvtKey = async() => {
-    if (ActiveWallet?.address){
-await window.navigator.clipboard.writeText(ActiveWallet?.privateKey);
- alert("Copied pvt key to clipboard ")
-    }   
-  }
+  let createNewWallet = () => {
+    let newWallet = Wallet.generate();
+    setActiveWallet(newWallet);
+    localStorage.setItem("ActiveWallet", newWallet.address.toString());
+    localStorage.setItem("PvtKey", newWallet.privateKey);
+  };
+
+  let copyPvtKey = async () => {
+    if (ActiveWallet?.address) {
+      await window.navigator.clipboard.writeText(ActiveWallet?.privateKey);
+      alert("Copied pvt key to clipboard ");
+    }
+  };
   return (
     <div className="App">
       <header className="App-header">
         <Routes>
-          <Route path="/" element={
-          <div className="Body-wrapper">
-        
-        {isReturnUser && <p> Welcome Back! </p>}
-        {ActiveWallet &&<><p> Active Wallet Address:</p> <p>{JSON.stringify(ActiveWallet.address).slice(4, -1)}</p></>}
-        <button onClick={() =>  createNewWallet()}>
-          New Wallet
-        </button>
-        <button onClick={() =>  copyPvtKey()}> Export Pvt Key</button>
-        </div>}/>
-        <Route path="market" element={<Market/>}/>
+          <Route
+            path="/"
+            element={
+              <div className="Body-wrapper">
+                {isReturnUser && <p> Welcome Back! </p>}
+                {ActiveWallet && (
+                  <>
+                    <p> Active Wallet Address:</p>{" "}
+                    <p>{JSON.stringify(ActiveWallet.address).slice(4, -1)}</p>
+                  </>
+                )}
+                <button onClick={() => createNewWallet()}>New Wallet</button>
+                <button onClick={() => copyPvtKey()}> Export Pvt Key</button>
+              </div>
+            }
+          />
+          <Route path="market" element={<Market />} />
+          <Route path="inventory" element={<Inventory />} />
         </Routes>
       </header>
-      
     </div>
   );
 }
--- frontend/src/components/Inventory.tsx
+++ frontend/src/components/Inventory.tsx
@@ -1,11 +1,44 @@
-import React, {useState, useEffect} from "react";
-
+import React, { useState, useEffect } from "react";
 
 function Inventory() {
-    return(
-     <>
-    <div>Inventory</div>
-    </>)
+  const [inventory, setInventory] = useState("");
+  let results = "someresults";
+
+  useEffect(() => {
+    const url = "http://127.0.0.1:8080/items/";
+const body = {
+    'wallet': "fuel1cvwcrwsl09krl0dfqtttcemht483yux3t0mmh8xmpaz26edn6rrszg706m",
+    'item': 1323,
+    'amount': 1
+}
+    const fetchData = async () => {
+      
+      try {
+        const response = await fetch(url, {
+            mode: 'no-cors',
+          headers: {'Accept': '*/*',
+      'Content-Type': 'application/json'},
+          method: "POST",
+          body: JSON.stringify(body),
+        });
+        const json = await response.json();
+        console.log(json);
+        setInventory(json);
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+  return (
+    <>
+      <div className="Inventory-Wrapper">
+        <h1>Inventory:</h1>
+        <p>{results}</p>
+      </div>
+    </>
+  );
 }
 
 export default Inventory;
